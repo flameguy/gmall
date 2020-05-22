1c50cbc30d70420b632c16dc1a7fce73d3e980f2
ybxu <ybxu@example.com>
2020-05-22 20:33:37 +0800

    项目结构创建

---
+++ gmall-oms/src/main/java/com/xyb/gmall/gmall/oms/GmallOmsApplication.java
@@ -0,0 +1,13 @@
+package com.xyb.gmall.gmall.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmallOmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallOmsApplication.class, args);
+    }
+
+}
